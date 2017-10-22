28710903a83f32d00fb31172790a03c78ddaaca9
Juan Guaba <wava0324@gmail.com>
2017-10-21 22:31:37 -0500

    a

--- EjercicioPilasyColas/Cola.h
+++ EjercicioPilasyColas/Cola.h
@@ -7,51 +7,61 @@
 
 using namespace std;
 
-class cola{nodo *cab,*fin;
-      public: cola(){cab=fin=NULL;}
-      void InsCola(char i);
-      char AtenderCola();
-      void ImprimirCola();
-      bool ColaVacia();
-      ~cola();
- };
+class cola{
+	nodo *cab, *fin;
+    public: cola(){
+		cab=fin=NULL;
+	}
+	void InsCola(char i);
+	char AtenderCola();
+    void ImprimirCola();
+    bool ColaVacia();
+    ~cola();
+};
+
 void cola::InsCola(char i){
-     nodo *nuevo;
-     nuevo= new nodo;
-     nuevo->dato=i;
-     nuevo->sig= NULL;
-     if (cab==NULL)
-         {cab=nuevo;}
-     else {fin->sig = nuevo;}
-     fin=nuevo;
+    nodo *nuevo;
+    nuevo = new nodo;
+    nuevo->dato = i;
+    nuevo->sig = NULL;
+    if (cab == NULL){
+		cab = nuevo;
+	}else{
+		fin->sig = nuevo;
+	}
+    fin = nuevo;
 }
-char cola::AtenderCola()
-{   char x;
+
+char cola::AtenderCola(){
+	char x;
     nodo *aux = cab; 
-    cab=aux->sig;
-    x=aux->dato;
+    cab = aux->sig;
+    x = aux->dato;
     delete aux;
     return x;
-    }
+}
+
 void cola::ImprimirCola(){
-     nodo *aux;
-     aux=cab;
-     while(aux!=NULL){
-       cout<<aux->dato<<" ";
-       aux=aux->sig;}    
+    nodo *aux;
+    aux = cab;
+    while(aux != NULL){
+    	cout<<aux->dato<<" ";
+    	aux = aux->sig;
+	}    
 }
 
 bool cola::ColaVacia(){
-     return (cab==NULL);
-     }
+    return (cab == NULL);
+}
 
 cola::~cola(){
-     nodo *aux;
-     while(cab!=NULL)
-       {aux= cab;
-        cab=aux->sig;
-        delete aux;}
-     delete cab;
-     }
+    nodo *aux;
+    while(cab != NULL){
+		aux = cab;
+    	cab = aux->sig;
+        delete aux;
+	}
+    delete cab;
+}
 
 #endif
